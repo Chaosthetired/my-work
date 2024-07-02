2aadbbd1a9d144768e85eb71ec88de246dc06f56
Chaosthetired <156306479+Chaosthetired@users.noreply.github.com>
2024-07-02 12:07:50 +0200

    Add files via upload

---
+++ I.o.T/Lesbrief 2/programmans/python 2.2 lesbrief.py
@@ -0,0 +1,9 @@
+### Illias Bourjila
+### Opdracht 2.2
+print("hallo, dit is de pi!")
+name = "Illias"
+print("wat is your name")
+print("mijn naam is", name,)
+print("en wat is je leefetijd")
+age = "17"
+print ("mijn naam is", name,"and mijn leefe tijd is",age,)
